d41ca4efeaed7a82211139d8b51bda419df005e8
wangzhizhou <wangzhizhou@bytedance.com>
2023-11-09 13:57:19 +0800

    Initial Commit

---
+++ LiveActivityDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveActivityDemo
+//
+//  Created by joker on 2023/11/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiveActivityDemo/LiveActivityDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityDemoApp.swift
+//  LiveActivityDemo
+//
+//  Created by joker on 2023/11/9.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivityDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
